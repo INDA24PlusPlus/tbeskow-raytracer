ef0ef10b3d1dba5be1463daa1440ae1d7a2f82ba
TheodorBeskow <theodor.beskow@icloud.com>
2024-12-14 13:42:40 +0100

    Can now trace on GPU

--- kernel_code.cl
+++ kernel_code.cl
@@ -1,4 +1,3 @@
-
 typedef struct{
     float3 origin;
     float3 direction;
@@ -35,12 +34,10 @@ float intersect_sphere(Ray ray, float3 Os, float r){
     return (-b-sqrt(toRoot))/(2*a);
 }
 
-kernel void intersect(global Ray *rays, global Object *objects, int amObjects, global float3 *result){
-    int id = get_global_id(0);
-    Ray ray = rays[id];
+float2 intersect(Ray ray, global Object *objects, int amObjects){
 
     float min_dist = INFINITY;
-    float3 color = (float3)(0, 0, 0);
+    int min_index = -1;
     for(int i = 0; i<amObjects; i++){
         Object object = objects[i];
         float dist = -1;
@@ -49,16 +46,62 @@ kernel void intersect(global Ray *rays, global Object *objects, int amObjects, g
         if(dist <1e-8) continue;
         if(dist<min_dist){
             min_dist = dist;
-            color = object.color;
+            min_index = i;
         }
     }
-    result[id] = color;
+    return (float2)(min_index, min_dist);
 }
 
-// kernel void intersect(global Ray *rays, global Object *objects, int amObjects, global float4 *result){
-//     int id = get_global_id(0);
-//     if(id == 0) printf("Ray: (%f, %f, %f)\n", rays[0].direction.x, rays[0].direction.y, rays[0].direction.z);
-//     if(id == 0) printf("Ray: (%f, %f, %f)\n", rays[1].direction.x, rays[1].direction.y, rays[1].direction.z);
-//     if(id == 0) printf("Ray: (%f, %f, %f)\n", rays[2].direction.x, rays[2].direction.y, rays[2].direction.z);
-//     result[id] = (float4)(objects[0].color, 0); 
-// }
+float randomValue(uint state){ // borde kanske testa (igentligen ska state ut till nästa test)
+    state = state * 747796405 + 2891336453;
+    uint result = ((state >> ((state>>28)+4)) ^ state) * 277803737;
+    result = (result >> 22) ^ result;
+    return ((float)result / (float)UINT_MAX-0.5)*2.;
+}
+
+float3 randomDirection(float3 normal, uint state){
+    while(true){
+        state = state*747796405 + 2891336453;
+        float3 direction = (float3)(randomValue(state), randomValue(state+1), randomValue(state+2));
+        if(length(direction)>1) continue;
+        if(dot(direction, normal) < 0) continue;
+        return normalize(direction);
+    }
+}
+
+float3 findNormal(float3 origin, global Object *objects, int index){
+    if(objects[index].type == 0) return normalize(origin-objects[index].center);
+    else if(objects[index].type == 1) return  objects[index].normal;
+
+    return (float3)(0, 0, 0);
+}
+
+
+kernel void trace(global Ray *rays, global Object *objects, int amObjects, int depth, int amount, global float4 *result){
+    int id = get_global_id(0);
+    Ray ray = rays[id];
+
+    float3 res = (float3)(0, 0, 0);
+    Ray originalRay = ray;
+    for(int iteration = 0; iteration<amount; iteration++){
+        float3 incomingColor = (float3)(0, 0, 0);
+        float3 rayColor = (float3)(1, 1, 1);
+        ray = originalRay;
+
+        for(int i = 0; i<depth; i++){
+            float2 intersection = intersect(ray, objects, amObjects);
+            int index = intersection.x;
+            float distance = intersection.y;
+            if(index == -1) break;
+            ray.origin += ray.direction*distance;
+            ray.direction = randomDirection(findNormal(ray.origin, objects, index), id*depth*2+i+iteration*291383);
+
+            incomingColor+=objects[index].color*objects[index].luminance*rayColor;
+            rayColor *= objects[index].color;
+        }
+        res += incomingColor;
+    }
+
+    result[id] = (float4)(res/amount, 0);
+}
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 from PIL import Image
-from multiprocessing import Pool, cpu_count
-from functools import partial
+import pyopencl as cl
 import time
 
 class camera:
@@ -15,128 +14,101 @@ class camera:
         up = up / (np.linalg.norm(up)*max(width, height))
         self.grid = [[self.direction+(x-width/2)*right+(y-height/2)*up for x in range(width)] for y in range(height)]
 
-    def processPixel(self, coord, objects):
-        x, y = coord
-        point = self.grid[y][x]
-        result = np.array([0, 0, 0], dtype=float)
-        amount = 30
-        for _ in range(amount):
-            r = ray(np.array([0, 0, 0]), point/np.linalg.norm(point))
-            result += r.trace(objects)
-        return self.width-x-1, self.height-y-1, tuple((result/amount*255).astype(int)) 
+        self.platform = cl.get_platforms()[0]
+        self.device = self.platform.get_devices()[0]
+        self.context = cl.Context([self.device])
+        self.queue = cl.CommandQueue(self.context)
 
-    def render(self, objects, amountProcesses = cpu_count()):
+        with open("kernel_code.cl", "r") as f:
+            self.program = cl.Program(self.context, f.read()).build()
 
-        coords = [[x, y] for x in range(self.width) for y in range(self.height)]
-        pixelFunc = partial(self.processPixel, objects=objects)
-            
-        print(f"Processes: {amountProcesses}")
-        with Pool(processes=amountProcesses) as pool:
-            res = pool.map(pixelFunc, coords)
+
+    def render(self, preObjects):
+
+        rays = np.array([(np.array([0., 0., 0., 0.]), np.concatenate([point/np.linalg.norm(point), [0.]]))
+                         for row in self.grid for point in row],
+                         dtype=[
+                            ("origin", "f4", 4),
+                            ("direction", "f4", 4),
+                         ])
+        
+        objects = np.array([(
+            np.int32(0 if obj.type == "sphere" else 1),
+            np.float32(obj.luminance),
+            np.float32(obj.radius if obj.type == "sphere" else 0),
+            np.float32(obj.distance if obj.type == "plane" else 0),
+            np.concatenate([obj.color, [0.0]]).astype(np.float32),
+            np.concatenate([obj.emittedColor, [0.0]]).astype(np.float32),
+            np.concatenate([obj.center, [0.0]]).astype(np.float32) if obj.type == "sphere" else np.zeros(4, dtype=np.float32),
+            np.concatenate([obj.normal, [0.0]]).astype(np.float32) if obj.type == "plane" else np.zeros(4, dtype=np.float32),
+        ) for obj in preObjects],
+        dtype=np.dtype([
+            ("type", "i4"),
+            ("luminance", "f4"),
+            ("radius", "f4"),
+            ("distance", "f4"),
+            ("color", "f4", 4),
+            ("emittedColor", "f4", 4),
+            ("center", "f4", 4),
+            ("normal", "f4", 4),
+        ], align=True))
+
+
+        results = np.zeros(len(rays), dtype=np.dtype("f4, f4, f4, f4"))
+        
+        mf = cl.mem_flags
+        rays_buf = cl.Buffer(self.context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=rays)
+        objects_buf = cl.Buffer(self.context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=objects)
+        results_buf = cl.Buffer(self.context, mf.WRITE_ONLY, results.nbytes)
+
+
+        depth = 10
+        amount = 10000
+        self.program.trace(self.queue, (len(rays),), None, rays_buf, objects_buf, np.int32(len(objects)), np.int32(depth), np.int32(amount), results_buf)
+
+        cl.enqueue_copy(self.queue, results, results_buf)
 
         img = Image.new('RGB', (self.width, self.height))
         pixels = img.load()
-        for x, y, color in res: pixels[x, y] = color
+
+        for i, (r, g, b, _) in enumerate(results):
+            x, y = i % self.width, i // self.width
+
+            pixels[self.width-x-1, self.height-y-1] = tuple((int(r*255), int(g*255), int(b*255)))
 
         img.save('render.png')
-        
+
 
 class object:
     def __init__(self, type, params, color = (1, 1, 1), emittedColor = (1, 1, 1), luminance = 0):
         self.type = type
-        self.color = np.array(color, dtype=float)
-        self.emittedColor = np.array(emittedColor, dtype=float)
+        self.color = np.array(color, dtype=np.float32)
+        self.emittedColor = np.array(emittedColor, dtype=np.float32)
         self.luminance = luminance
         if type == "sphere": # ensidig
-            self.center = np.array(params[0], dtype=float)
-            self.radius = np.array(params[1], dtype=float)
+            self.center = np.array(params[0], dtype=np.float32)
+            self.radius = params[1]
         elif type == "plane": # ensidig
             # self.point = np.array(params[0], dtype=float)
-            self.normal = np.array(params[1], dtype=float)
+            self.normal = np.array(params[1], dtype=np.float32)
             self.distance = np.dot(params[0], self.normal)
         # elif type == "triangle":
         #     self.vertices = params
         else:
             raise Exception("Object does not exist")
-
-
-class ray:
-    def __init__(self, origin, direction):
-        self.origin = np.array(origin, dtype=float)
-        self.direction = np.array(direction, dtype=float)
-
-
-
-    def trace(self, objects, depth = 10):
-        incomingColor = np.array([0, 0, 0], dtype=float)
-        rayColor = np.array([1, 1, 1], dtype=float)
-        for _ in range(depth):
-            result, distance = self.intersect(objects)
-            if result == None: break
-            self.origin+=self.direction*distance
-            self.direction = self.randomDirection(self.findNormal(result))
-
-            incomingColor+=result.emittedColor*result.luminance*rayColor
-            rayColor *= result.color
-
-
-        return incomingColor
-    
-    def randomDirection(self, normal):
-        while True:
-            direction = np.random.uniform(-1, 1, 3)
-            if np.linalg.norm(direction) > 1: continue
-            if np.dot(direction, normal) < 0: continue
-            return direction/np.linalg.norm(direction)
-    
-    def findNormal(self, object):
-        if object.type == "sphere":
-            return (self.origin-object.center)/object.radius
-        elif object.type == "plane":
-            return object.normal
-        else:
-            raise Exception("gg")
-
-    def intersect(self, objects): 
-        min_dist = np.inf
-        result = None
-        for object in objects:
-            if object.type == "sphere":
-                dist = self.intersect_sphere(object.center, object.radius)
-            elif object.type == "plane":
-                dist = self.intersect_plane(object.normal, object.distance)
-            if dist <= 1e-8: continue
-            if dist < min_dist:
-                min_dist = dist
-                result = object
-        return result, min_dist
-
-    def intersect_sphere(self, Os, r):
-        v = self.origin - Os
-        a = np.sum(self.direction*self.direction)
-        b = 2*np.sum(v*self.direction)
-        c = np.sum(v*v)-r*r
-        toRoot = b**2-4*a*c
-        if toRoot < 0: return -1
-        return (-b-np.sqrt(toRoot))/(2*a)
-    
-    def intersect_plane(self, N, distance):
-        scalar = np.dot(self.direction, N)
-        if scalar == 0: return -1
-        return (distance-np.dot(self.origin, N))/scalar
-    
+        
 
 def main():
-    width, height = 192, 108
+    width, height = 192*2, 108*2
     # width, height = 50, 40
     cam = camera(width, height)
     objects = [
-        object("sphere", [np.array([0, 0, 4]), 1], (1, 0, 0), 0), 
-        object("sphere", [np.array([1.1, 0, 4.5]), .5], (.3, 0, 1), 0), 
-        object("sphere", [np.array([-9, 25, 14]), 20], luminance=1), 
-        object("plane", [np.array([0, -1, 0]), np.array([0, 1, 0])], (0, 1, 0), 0)
+        object("sphere", [np.array([0, 0, 4]), 1.], (1, 0, 0)), 
+        object("sphere", [np.array([1.1, 0, 4.5]), .5], (.3, 0, 1)), 
+        object("sphere", [np.array([-9, 25, 14]), 20.], luminance=1), 
+        object("plane", [np.array([0, -1, 0]), np.array([0, 1, 0])], (0, 1, 0))
     ]
-    cam.render(objects, amountProcesses=8)
+    cam.render(objects)
 
 
 if __name__ == "__main__":
